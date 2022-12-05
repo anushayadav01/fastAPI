4c46b0afdce15aef98760beb2b8da8a6efa6e16b
anushayadav01 <anusha_yadav@mentor.com>
2022-12-05 12:45:46 +0530

    added files

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#-------------------------------------------------------------
+#Setup script for MTDA-MQTT
+#-------------------------------------------------------------
+from setuptools import setup, find_packages
+
+setup(
+    name='mtda_mqtt',
+    packages=find_packages(),
+    version='1.0.0',
+    url='https://github.com/anushayadav01/mtda-mqtt',
+    classifiers=[
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+
+    install_requires=[
+        "python-daemon>=2.0",
+        "fastapi_websocket_rpc"
+    ],
+)
